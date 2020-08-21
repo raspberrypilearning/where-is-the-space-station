0bcd0a6ad99d4e7d5ea68d4e045756d39c103412
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-08-21 12:29:54 +0100

    New translations iss-finished.py (French)

--- fr-FR/solutions/iss-finished.py
+++ fr-FR/solutions/iss-finished.py
@@ -7,41 +7,41 @@ import time
 
 # http://open-notify.org/Open-Notify-API/
 url = 'http://api.open-notify.org/astros.json'
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
+reponse = urllib.request.urlopen(url)
+resultat = json.loads(reponse.read())
 
-print('People in Space: ', result['number'])
+print('Personnes dans l''espace: ', resultat['number'])
 
-people = result['people']
+personnes = resultat['people']
 
-for p in people:
-  print(p['name'], ' in ', p['craft'])
+for p in personne:
+  print(p['name'], ' dans ', p['craft'])
 
 
 url = 'http://api.open-notify.org/iss-now.json'
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
+reponse = urllib.request.urlopen(url)
+resultat = json.loads(reponse.read())
 
-location = result['iss_position']
-lat = float(location['latitude'])
-lon = float(location['longitude'])
+emplacement = resultat['iss_position']
+lat = float(emplacement['latitude'])
+lon = float(emplacement['longitude'])
 print('Latitude: ', lat)
 print('Longitude: ', lon)
 
-screen = turtle.Screen()
-screen.setup(720, 360)
-screen.setworldcoordinates(-180, -90, 180, 90)
-screen.bgpic('map.gif')
+ecran = turtle.Screen()
+ecran.setup(720, 360)
+ecran.setworldcoordinates(-180, -90, 180, 90)
+ecran.bgpic('map.gif')
 
 
-screen = turtle.Screen()
-screen.setup(720, 360)
-screen.setworldcoordinates(-180, -90, 180, 90)
-# image source:
-# map.jpg: http://visibleearth.nasa.gov/view.php?id=57752 Credit: NASA
-screen.bgpic('map.gif')
+ecran = turtle.Screen()
+ecran.setup(720, 360)
+ecran.setworldcoordinates(-180, -90, 180, 90)
+# source de l'image:
+# map.jpg: http://visibleearth.nasa.gov/view.php?id=57752 Crédit: NASA
+ecran.bgpic('map.gif')
 
-screen.register_shape('iss.gif')
+ecran.register_shape('iss.gif')
 iss = turtle.Turtle()
 iss.shape('iss.gif')
 iss.setheading(90)
@@ -49,7 +49,7 @@ iss.setheading(90)
 iss.penup()
 iss.goto(lon, lat)
 
-# When Does ISS next pass over me?
+# Quand ISS passe-t-il ensuite au-dessus de moi ?
 #london
 #lat = 51.5072
 #lon = 0.1275
@@ -58,24 +58,24 @@ iss.goto(lon, lat)
 #lat = 35.689487
 #lon = 139.691706
 
-# Space Center, Houston
+# Centre Spatial, Houston
 lat = 29.5502
 lon = -95.097
 
-location = turtle.Turtle()
-location.penup()
-location.color('yellow')
-location.goto(lon,lat)
-location.dot(5)
-location.hideturtle()
+emplacement = turtle.Turtle()
+emplacement.penup()
+emplacement.color('yellow')
+emplacement.goto(lon,lat)
+emplacement.dot (5)
+hideturtle()
 
 url = 'http://api.open-notify.org/iss-pass.json?lat=' + str(lat) + '&lon=' + str(lon)
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
+reponse = urllib.request.urlopen(url)
+resultat = json.loads(reponse.read())
 
-#print result
-over = result['response'][1]['risetime']
-location.write(time.ctime(over))
+#print resultat
+audessus = result['response'][1]['risetime']
+emplacement.write(time.ctime(audessus))
 
 
 
